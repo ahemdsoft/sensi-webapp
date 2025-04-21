c55e68d67c9cf065b79000a253451e6aaff911ad
ahemdsoft <anasahmed4484@gmail.com>
2025-04-21 14:26:52 +0600

    anas

---
+++ app/about-us/[id]/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { useParams } from "next/navigation";
+export default function localPage() {
+    const params = useParams();
+    const {id} = params;
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1>this is {id} page</h1>
+            
+        </div>
+    )
+}
+
